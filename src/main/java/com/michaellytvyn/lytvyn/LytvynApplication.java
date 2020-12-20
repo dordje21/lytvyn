a9d05c759f97bb65a4f8a2492d229e7ddfc2b01e
Michael <cresedel211194@gmail.com>
2020-12-20 23:30:05 +0200

    hw28 spring start

---
+++ src/main/java/com/michaellytvyn/lytvyn/LytvynApplication.java
@@ -0,0 +1,13 @@
+package com.michaellytvyn.lytvyn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LytvynApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LytvynApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/michaellytvyn/lytvyn/LytvynApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michaellytvyn.lytvyn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LytvynApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
